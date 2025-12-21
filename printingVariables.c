51915333d64205ab0647beb01efb337b9602987c
Riya agrawal <riyaagrawal2712@gmail.com>
2025-12-21 15:18:52 +0530

    Add files via upload

---
+++ printingVariables.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int age=25;
+    float price=25.0;
+    char star='*';
+    printf("age is %d",age);
+    printf("\nprice is : %f",price);
+    printf("\nstar is: %c",star);
+    return 0;
+}
