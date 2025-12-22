06140b61004ff2fa0bb6bc96282dc6fc4896d526
Riya agrawal <riyaagrawal2712@gmail.com>
2025-12-22 15:08:03 +0530

    Add files via upload

---
+++ sumOfTwoNumbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf("enter first number:");
+    scanf("%d",&a);
+    printf("enter second number:");
+    scanf("%d",&b);
+    int sum=a+b;
+    printf("sum is:%d",sum);
+    return 0;
+}
