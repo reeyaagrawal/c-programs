3db9b621778d429c72a6e2170daf3372d0a6262d
Riya agrawal <riyaagrawal2712@gmail.com>
2025-12-23 16:57:32 +0530

    Add files via upload

---
+++ associativityPrecedence.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int a=5*2-2*3;//(5*2)-(2*3)=10-6=4
+    int b=5*2/2*3;//((5*2)/2)*3=(10/2)*3=5*3=15
+    int c=5*(2/2)*3;//5*1*3=15
+    int d=5+2/2*3;//5+1*3=5+3=8
+    printf("a= %d",a);
+    printf("\nb= %d",b);
+    printf("\nc= %d",c);
+    printf("\nd= %d",d);
+    return 0;
+}
