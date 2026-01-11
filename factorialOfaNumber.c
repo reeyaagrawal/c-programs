7fa7eb57d21f7936b34311ca61d70c093d33f5e1
Riya agrawal <riyaagrawal2712@gmail.com>
2026-01-11 16:45:40 +0530

    Add files via upload

---
+++ factorialOfaNumber.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int fact=1;
+    printf("enter number: ");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        fact*=i;
+    }
+    printf("factorial is: %d",fact);
+    return 0;
+}
