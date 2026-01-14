73b08f392cca56f8f5a04f23944ff381f577d276
Riya agrawal <riyaagrawal2712@gmail.com>
2026-01-14 12:42:41 +0530

    Add files via upload

---
+++ introducingFunction.c
@@ -0,0 +1,14 @@
+//write two function:
+//one to print hello and second to print goodbye
+#include <stdio.h>
+void printHello () {
+    printf("Hello! \n");
+}
+void printGoodbye () {
+    printf ("Goodbye:)\n ");
+}
+int main() {
+    printHello() ;
+    printGoodbye() ;
+    return 0;
+}
