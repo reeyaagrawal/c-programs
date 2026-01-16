b4a434f9f14212308cb8638b30a8739315697291
Riya agrawal <riyaagrawal2712@gmail.com>
2026-01-16 11:52:55 +0530

    Add files via upload

---
+++ printTableViaFunctions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void printTable(int n){
+    for(int i=1;i<=10;i++){
+        printf("%d * %d = %d\n",n,i,n*i);
+    }
+}
+int main(){
+    int n;
+    printf("enter number: ");
+    scanf("%d",&n);
+    printTable(n);
+    return 0;
+}
